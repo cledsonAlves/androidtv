45a645b002334465b8a67596afa9a1176b1f4f9a
Your Name <you@example.com>
2023-09-07 22:46:48 -0300

    auto

---
+++ app/src/main/java/com/example/myapplication/Home.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.util.Log
+
+class Home {
+    private fun addHome(){
+        val content = "connect"
+        if (content == "connect"){
+              Log.ASSERT
+        }
+        addHome()
+    }
+}
